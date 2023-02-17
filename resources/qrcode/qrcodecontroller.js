9b28e768e89da59e88147bd80389cca205608eaa
tazimmadre <tazimmadre5041@gmail.com>
2023-02-17 16:53:24 +0530

    send all data , and add validation

--- resources/qrcode/qrcodecontroller.js
+++ resources/qrcode/qrcodecontroller.js
@@ -1,12 +1,16 @@
 import { QRcodeData } from "./qrcodedata.js";
 
 const postQrcodeData = async (req, res) => {
+  if(!req.body){
+    return res.status(400).send({data:[], status:'ERROR', message: 'Add Data to store.'});
+  }
   const data = await QRcodeData.create(req.body);
   res.send({ data, status: "OK" });
 };
 
 const getQrcodeData = async (req, res) => {
-  const data = await QRcodeData.find({}).sort({ createdAt: -1 }).limit(1);
+  const data = await QRcodeData.find({}).sort({ createdAt: -1 });
+  // .sort({ createdAt: -1 }).limit(1);
   res.send({ data, status: "OK" });
 };
 
